9dc86e3d45a5eebb02ebc487067495f8fc3c8832
bendeezy <benndego@gmail.com>
2025-09-28 06:56:20 +0000

    MissionII

---
+++ mission/mission.js
@@ -0,0 +1,20 @@
+// Select important elements
+const themeSelector = document.querySelector('#theme');
+const body = document.body;
+const logo = document.getElementById('logo');
+
+// Function to handle theme changes
+function changeTheme() {
+  const theme = themeSelector.value;
+
+  if (theme === 'dark') {
+    body.classList.add('dark'); /
+    logo.src = 'byui-logo_white.png'; 
+  } else {
+    body.classList.remove('dark'); 
+    logo.src = 'byui-logo_blue.png'; 
+  }
+}
+
+
+themeSelector.addEventListener('change', changeTheme);
